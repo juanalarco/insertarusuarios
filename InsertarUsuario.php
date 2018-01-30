8686c12a1b05b3f92c43422110beeba50595513a
Juan Alarcon Garijo <jualga01@floridauniversitaria.es>
2018-01-30 12:33:21 +0100

    insertar usuarios php

---
+++ InsertarUsuario.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>formulario</title>
+  </head>
+  <body>
+
+    <form action="listadoUsuario.php" method="post">
+
+      	Nombre:<br>
+      	<input type="text" name="nombre">
+
+        <br><br>
+
+      	Apellidos:<br>
+      	<input type="text" name="apellidos">
+
+      	<br><br>
+
+        Edad:<br>
+        <input type="text" name="edad">
+
+        <br><br>
+
+        Curso:<br>
+      	<input type="text" name="curso">
+
+      	<br><br>
+
+        Puntuacion:<br>
+      	<input type="text" name="puntuacion">
+
+      	<br><br>
+
+
+
+      	<input type="submit" value="Insertar">
+    	</form>
+
+  </body>
+</html>
